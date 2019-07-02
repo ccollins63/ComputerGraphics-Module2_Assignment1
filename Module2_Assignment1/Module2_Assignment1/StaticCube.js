95f053eb4e8dfe3a73cb1e797508449b33e34f9b
Cam Collins <33230098+ccollins63@users.noreply.github.com>
2019-07-02 12:07:28 -0400

    Add files via upload

---
+++ Module2_Assignment1/Module2_Assignment1/StaticCube.js
@@ -0,0 +1,188 @@
+﻿/*
+ * Course: CS 4722
+ * Section: W01
+ * Name: Cameron Collins
+ * Professor: Dr. Shaw
+ * Assignment #: Assignment #1
+*/
+
+"use strict";
+
+var canvas;
+var gl;
+
+var theta = vec3( 45, 45, 45 );
+var thetaLoc;
+
+var vertices = [
+        vec3( -0.5,  0.5,  0.5 ),   //vertex 1
+        vec3( -0.5, -0.5,  0.5 ),   //vertex 0
+        vec3(  0.5, -0.5,  0.5 ),   //vertex 3
+
+        vec3(  0.5, -0.5,  0.5 ),   //vertex 3
+        vec3(  0.5,  0.5,  0.5 ),   //vertex 2
+        vec3( -0.5,  0.5,  0.5 ),   //vertex 1
+
+        vec3(  0.5,  0.5,  0.5 ),   //vertex 2
+        vec3(  0.5, -0.5,  0.5 ),   //vertex 3
+        vec3(  0.5, -0.5, -0.5 )    //vertex 7
+
+        vec3(  0.5, -0.5, -0.5 )    //vertex 7
+        vec3(  0.5,  0.5, -0.5 ),   //vertex 6
+        vec3(  0.5,  0.5,  0.5 ),   //vertex 2
+
+        vec3(  0.5, -0.5,  0.5 ),   //vertex 3
+        vec3( -0.5, -0.5,  0.5 ),   //vertex 0
+        vec3( -0.5, -0.5, -0.5 ),   //vertex 4
+
+        vec3( -0.5, -0.5, -0.5 ),   //vertex 4
+        vec3(  0.5, -0.5, -0.5 )    //vertex 7
+        vec3(  0.5, -0.5,  0.5 ),   //vertex 3
+
+        vec3(  0.5,  0.5, -0.5 ),   //vertex 6
+        vec3(  0.5,  0.5,  0.5 ),   //vertex 2
+        vec3( -0.5,  0.5,  0.5 ),   //vertex 1
+
+        vec3( -0.5,  0.5,  0.5 ),   //vertex 1
+        vec3( -0.5,  0.5, -0.5 ),   //vertex 5
+        vec3(  0.5,  0.5, -0.5 ),   //vertex 6
+
+        vec3( -0.5, -0.5, -0.5 ),   //vertex 4
+        vec3( -0.5,  0.5, -0.5 ),   //vertex 5
+        vec3(  0.5,  0.5, -0.5 ),   //vertex 6
+
+        vec3(  0.5,  0.5, -0.5 ),   //vertex 6
+        vec3(  0.5, -0.5, -0.5 )    //vertex 7
+        vec3( -0.5, -0.5, -0.5 ),   //vertex 4
+
+        vec3( -0.5,  0.5, -0.5 ),   //vertex 5
+        vec3( -0.5, -0.5, -0.5 ),   //vertex 4
+        vec3( -0.5, -0.5,  0.5 ),   //vertex 0
+
+        vec3( -0.5, -0.5,  0.5 ),   //vertex 0
+        vec3( -0.5,  0.5,  0.5 ),   //vertex 1
+        vec3( -0.5,  0.5, -0.5 ),   //vertex 5
+
+    /*
+    vec3( -0.5, -0.5,  0.5 ),   //vertex 0
+    vec3( -0.5,  0.5,  0.5 ),   //vertex 1
+    vec3(  0.5,  0.5,  0.5 ),   //vertex 2
+    vec3(  0.5, -0.5,  0.5 ),   //vertex 3
+    vec3( -0.5, -0.5, -0.5 ),   //vertex 4
+    vec3( -0.5,  0.5, -0.5 ),   //vertex 5
+    vec3(  0.5,  0.5, -0.5 ),   //vertex 6
+    vec3(  0.5, -0.5, -0.5 )    //vertex 7
+    */
+];
+
+var vertexColors = [
+
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red     1
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black   0
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green   3
+
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green   3
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow  2
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red     1
+
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow  2
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green   3
+    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan    7
+
+    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan    7
+    vec4( 1.0, 1.0, 1.0, 1.0 ),  // white   6
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow  2
+
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green   3
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black   0
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue    4
+
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue    4
+    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan    7
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green   3
+
+    vec4( 1.0, 1.0, 1.0, 1.0 ),  // white   6
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow  2
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red     1
+
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red     1
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta 5
+    vec4( 1.0, 1.0, 1.0, 1.0 ),  // white   6
+
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue    4
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta 5
+    vec4( 1.0, 1.0, 1.0, 1.0 ),  // white   6
+
+    vec4( 1.0, 1.0, 1.0, 1.0 ),  // white   6
+    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan    7
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue    4
+
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta 5
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue    4
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black   0
+
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black   0
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red     1
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta 5
+
+  /*vec4( 0.0, 0.0, 0.0, 1.0 ),  // black   0
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red     1
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow  2
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green   3
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue    4
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta 5
+    vec4( 1.0, 1.0, 1.0, 1.0 ),  // white   6
+    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan    7*/
+];
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+
+    // vertex array attribute buffer code goes here
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    // color array attribute buffer code goes here
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    // thetaLoc uniform variable code goes here
+    thetaLoc = gl.getUniformLocation(program, "theta");
+    gl.uniform3fv(thetaLoc, theta);
+
+    render();
+}
+
+function render()
+{
+    // render code goes here
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+
+    gl.drawArrays(gl.TRIANGLES, 0, vertices.length);
+
+}
